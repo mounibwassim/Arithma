1274f0a8e89bc8aa7f8e9c20dded864b0b7c0c13
M.W <mounibwassim@users.noreply.github.com>
2026-03-22 23:54:27 +0800

    chore: remove console log watermark

--- src/app/(chat)/swr-config.tsx
+++ src/app/(chat)/swr-config.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { BasicUser } from "app-types/user";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { SWRConfig, type SWRConfiguration } from "swr";
 
 export function SWRConfigProvider({
@@ -22,12 +22,6 @@ export function SWRConfigProvider({
     };
   }, [user]);
 
-  useEffect(() => {
-    console.log(
-      "%c█▄▄ █▀▀ ▀█▀ ▀█▀ █▀▀ █▀█\n█▄█ █▄▄  █   █  █▄▄ █▀▄\n\n%c⛓️ Just a Better Chatbot\nhttps://github.com/cgoinglove/better-chatbot",
-      "color: #00d4ff; font-weight: bold; font-family: monospace; font-size: 16px; text-shadow: 0 0 10px #00d4ff;",
-      "color: #888; font-size: 12px;",
-    );
-  }, []);
+
   return <SWRConfig value={config}>{children}</SWRConfig>;
 }
